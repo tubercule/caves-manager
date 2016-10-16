50861706c3962eec763bc33ef3ad8db145c49593
Antoine <antoine@vittecoq.net>
2016-10-16 20:50:49 +0200

    Ajout des variables d'environnement pour AWS-EB

--- config/database.php
+++ config/database.php
@@ -1,5 +1,20 @@
 <?php
 
+
+if (isset($_SERVER['DBNAME'])) {
+    define(DBHOST, $_SERVER['DBHOSTNAME']);
+    define(DBPORT, $_SERVER['DBPORT']);
+    define(DBNAME, $_SERVER['DBNAME']);
+    define(DBUSENAME, $_SERVER['DBUSER']);
+    define(DBPASS, $_SERVER['DBPASSWORD']);
+} else {
+    define(DBHOST, env('DB_HOST', 'localhost'));
+    define(DBPORT, env('DB_PORT', '3306'));
+    define(DBNAME,  env('DB_DATABASE', 'forge'));
+    define(DBUSENAME,  env('DB_USERNAME', 'forge'));
+    define(DBPASS,  env('DB_PASSWORD', ''));
+}
+
 return [
 
     /*
@@ -54,11 +69,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => DBHOST,
+            'port' => DBPORT,
+            'database' => DBNAME,
+            'username' => DBUSER,
+            'password' => DBPASSWORD,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
